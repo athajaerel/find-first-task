a961b16523357ee323aed36f748bd64cc11dd752
Adam Richardson <adam.richardson@cgi.com>
2021-07-19 18:56:08 +0100

    Add -f option

--- find-first-task.sh
+++ find-first-task.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 set -euo pipefail
 
-# TODO: check for args - for each, do this
-grep "^- name:" roles/$1/tasks/main.yml | head -1 | cut -d\  -f3-
+F=$1
+S=$1
+
+if [ "x-f" == "x${F}" ]; then
+	S=$2
+fi
+
+TASK=$(grep "^- name:" roles/${S}/tasks/main.yml | head -1 | cut -d\  -f3-)
+
+if [ "x-f" == "x${F}" ]; then
+	TASK="${S} : ${TASK}"
+fi
+
+echo ${TASK}
