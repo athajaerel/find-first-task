b541d8fd6c9041dc48fc1ab090ce3ba56acfbe13
Adam Richardson <adam.richardson@cgi.com>
2021-07-26 09:18:58 +0100

    Check for missing role

--- find-first-task.sh
+++ find-first-task.sh
@@ -13,6 +13,10 @@ if [ "x-f" == "x${F}" ]; then
 	S=$2
 fi
 
+[ ! -d "roles/${S}" ] && die "No role found: ${S}"
+[ ! -d "roles/${S}/tasks" ] && die "No role found: ${S}"
+[ ! -e "roles/${S}/tasks/main.yml" ] && die "No role found: ${S}"
+
 TASK=$(grep "^- name:" roles/${S}/tasks/main.yml | head -1 | cut -d\  -f3-)
 
 if [ "x-f" == "x${F}" ]; then
